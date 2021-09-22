bbceb58cd5bb3982091ec67e49b762173e32cb28
sotomaque <sotomaque@gmail.com>
2021-09-22 16:58:25 -0700

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Enrique Sotomayor on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
